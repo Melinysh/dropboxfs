bf38fac988bbbe5ce723ea8b06b48609488e534a
Zander Hill <zander@xargs.io>
2019-06-15 13:03:30 +0100

    Add logging levels and work on reliable unmounting

--- main.go
+++ main.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"strings"
 
 	bazil "bazil.org/fuse"
 	"bazil.org/fuse/fs"
 	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox"
 	"github.com/dropbox/dropbox-sdk-go-unofficial/dropbox/files"
 	"github.com/melinysh/dropboxfs/fuse"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -22,6 +25,12 @@ func main() {
 	tokenFilePtr := flag.String("t", "", "Path to file that contains Dropbox access token")
 
 	flag.Parse()
+
+	logLevel := dropbox.LogOff
+	if *verbosePtr {
+		logLevel = dropbox.LogDebug
+		log.SetLevel(log.DebugLevel)
+	}
 
 	// demand mountpoint
 	if *mountpointPtr == "" {
@@ -34,50 +43,49 @@ func main() {
 	if *tokenFilePtr == "" {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter Dropbox access token: ")
-		token, err := reader.ReadString('\n')
+		t, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Unable to read input", err)
 			os.Exit(1)
 		}
+		token := strings.TrimSpace(t)
 		*tokenFilePtr = "./dropbox_token"
-		if err = ioutil.WriteFile(*tokenFilePtr, []byte(token[:len(token)-1]), 0600); err != nil {
+		if err = ioutil.WriteFile(*tokenFilePtr, []byte(token), 0600); err != nil {
 			fmt.Println("Unable to write dropbox token into", *tokenFilePtr, err)
 			os.Exit(1)
 		}
-		fmt.Printf("Saved your token to %v\ndropboxfs can use this file later by providing the flag `-t %v`\n", *tokenFilePtr, *tokenFilePtr)
+		log.Printf("Saved your token to %v\ndropboxfs can use this file later by providing the flag `-t %v`\n", *tokenFilePtr, *tokenFilePtr)
 	}
 
 	tokenData, err := ioutil.ReadFile(*tokenFilePtr)
 	if err != nil {
-		log.Println("Unable to open token file", *tokenFilePtr, err)
+		log.Fatalln("Unable to open token file", *tokenFilePtr, err)
 	}
-	token := string(tokenData)
+	token := string(bytes.TrimSpace(tokenData))
 
-	log.Println("Will try to mount to mountpoint", *mountpointPtr)
+	log.Infoln("Will try to mount to mountpoint", *mountpointPtr)
 	c, err := bazil.Mount(*mountpointPtr)
 	if err != nil {
-		log.Fatal("Unable to mount:", err)
+		log.Fatalln("Unable to mount:", err)
 	}
-
-	log.Println("Mount successful!")
+	log.Infoln("Mount successful!")
 	defer c.Close()
 	<-c.Ready
 	// Check if the mount process has an error to report.
 	if err := c.MountError; err != nil {
-		log.Panicln("Error from mount point:", err)
+		log.Fatalln("Error from mount point:", err)
 	}
 	if p := c.Protocol(); !p.HasInvalidate() {
-		log.Panicln("kernel FUSE support is too old to have invalidations: version %v", p)
+		log.Fatalln("kernel FUSE support is too old to have invalidations: version %v", p)
 	}
+	// Cleanup the mount point in case of crashes
+	defer bazil.Unmount(*mountpointPtr)
 
-	logLevel := dropbox.LogOff
-	if *verbosePtr {
-		logLevel = dropbox.LogDebug
-	}
 	config := dropbox.Config{
 		Token:    token,
 		LogLevel: logLevel,
 	}
+
 	client := files.New(config)
 	rootDir := &fuse.Directory{
 		Metadata: &files.FolderMetadata{},
@@ -85,9 +93,10 @@ func main() {
 	db := fuse.NewDropbox(client, rootDir)
 
 	srv := fs.New(c, nil)
-	log.Println("Ready to serve FUSE")
-	if err := srv.Serve(db); err != nil {
-		log.Panicln("Unable to serve filesystem:", err)
+	log.Infoln("Ready to serve FUSE")
+	err = srv.Serve(db)
+	if err != nil {
+		log.Fatalln("Unable to serve filesystem:", err)
 	}
-	log.Println("Shutting down gracefully...")
+	log.Infoln("Shutting down gracefully...")
 }
